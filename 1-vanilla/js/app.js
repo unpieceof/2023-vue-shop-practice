a5479866955e37b8d2752566a9a8812942f84ae7
unpieceof <unpieceof@gmail.com>
2022-04-29 22:00:23 +0900

    [Update] app.js 오타 수정

--- 1-vanilla/js/app.js
+++ 1-vanilla/js/app.js
@@ -1,4 +1,4 @@
-import MainController from "./controllers/MainController";
+import MainController from "./controllers/MainController.js";
 
 //controller 호출 시점은 DOM이 모두 로딩되고 난 후 
 document.addEventListener('DOMContentLoaded', () => {
