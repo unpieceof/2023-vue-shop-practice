ec3aee4db95e2a93c9836dce7f4bcb717104b4c3
unpieceof <unpieceof@gmail.com>
2022-04-29 21:57:44 +0900

    [Update] app.js에서 MainController 호출

---
+++ 1-vanilla/js/app.js
@@ -0,0 +1,6 @@
+import MainController from "./controllers/MainController";
+
+//controller 호출 시점은 DOM이 모두 로딩되고 난 후 
+document.addEventListener('DOMContentLoaded', () => {
+    MainController.init()
+})
