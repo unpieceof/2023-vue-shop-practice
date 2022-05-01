4f4d673a91603bdc9f09d33a8a5fcfa030df539a
unpieceof <unpieceof@gmail.com>
2022-05-01 18:12:41 +0900

    [Update] MainController.js에서 FormView의 함수 호출

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,7 +1,10 @@
+import FormView from "../views/FormView"
+
 const tag = '[MainController]'
 
 export default{
     init() {
         console.log(tag, 'init()')
+        FormView.setup(document.querySelector('form')) // index.html의 form을 인자로 넘겨 줌
     }
 }
