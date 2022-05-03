01b66e59b3cfa1a47ab94ad886fe311e70f3ba4e
unpieceof <unpieceof@gmail.com>
2022-05-03 22:26:45 +0900

    [Update] FormView.js 검색어 입력 시 reset 버튼 보이는 기능 추가

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -10,10 +10,20 @@ FormView.setup = function (el) {
     this.inputEl = el.querySelector('[type=text]')
     this.resetEl = el.querySelector('[type=reset]')
     this.showResetBtn(false) // Reset 속성은 숨김 처리, true/false는 대소문자 가림
+    this.bindEvents()
 }
 
 FormView.showResetBtn = function(show = true) {
     this.resetEl.style.display = show ? 'block' : 'none' // show 상태면 css의 display 속성을 block, 아니면 none
+}
+
+// 검색어 입력 시 x 버튼 보이도록 하는 이벤트 매핑
+FormView.bindEvents = function() {
+    this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
+}
+
+FormView.onKeyup = function() {
+    this.showResetBtn(this.inputEl.value.length)
 }
 
 // Controller에서 사용하기 위해 export 필요
