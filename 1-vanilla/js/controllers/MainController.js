5189d64539c767987b1610ae64c77e8b6eae82b9
unpieceof <unpieceof@gmail.com>
2022-05-05 23:57:55 +0900

    [Update] MainController.js onResetForm() 함수 추가

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -6,9 +6,14 @@ export default{
     init() {
         FormView.setup(document.querySelector('form')) // index.html의 form을 인자로 넘겨 줌
             .on('@submit', e => this.onSubmit(e.detail.input)) //FormView에서 전달한 값 받음
+            .on('@reset', e => this.onResetForm())
     },
 
     onSubmit(input) {
         console.log(tag, 'onSubmit()', input)
+    },
+
+    onResetForm() {
+        console.log(tag, 'onResetForm()')
     }
 }
