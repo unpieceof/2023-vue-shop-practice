005e66acd9d4335d5b8d61d15ad46ec2a4597e76
unpieceof <unpieceof@gmail.com>
2022-05-09 22:36:41 +0900

    [Update] ResultView.js 검색 결과에 따라 결과값을 그리는 render 함수 추가

--- 1-vanilla/js/views/ResultView.js
+++ 1-vanilla/js/views/ResultView.js
@@ -7,3 +7,15 @@ const ResultView = Object.create(View)
 ResultView.setup = function(el) {
     this.init(el)
 }
+
+ResultView.render = function(data = []){
+    console.log(tag, 'render()', data)
+    // 검색 결과(data)가 있으면 getSearchResultsHtml 함수를 통해 결과 그려줌
+    this.el.innerHTML = data.length ? this.getSearchResultsHtml(data) : '검색 결과가 없습니다'
+}
+
+ResultView.getSearchResultsHtml = function(data) {
+    debugger
+}
+
+export default ResultView
